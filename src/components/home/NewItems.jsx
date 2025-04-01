edd0cdf125d7ee96e950d42db21c6110094c2745
Adria <adriamooney@gmail.com>
2025-04-01 12:40:57 -0700

    new items feature done.  fetches new items from api and calculates time left before expiry which is displayed as a countdowntimer. implements react-slick carousel and uses skeleton loading state

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
 import axios from "axios";
 
 
 const NewItems = ({ Slider }) => {
   const [newItems, setNewItems] = useState([]);
-  const [timeLeft, setTimeLeft] = useState(new Date());
   const newItemsSettings = {
     dots: false,
     infinite: true,
@@ -39,15 +37,20 @@ const NewItems = ({ Slider }) => {
   };
 
   function formatTime(time) {
-   const timeLeft = time - Date.now();
-   const date = new Date(timeLeft * 1000);
-   const hours = date.getHours().toString();
-   const minutes = date.getMinutes().toString().padStart(2, '0');
-   const seconds = date.getSeconds().toString().padStart(2, '0');
-   console.log(`${hours}h ${minutes}m ${seconds}s`);
 
-   return `${hours}h ${minutes}m ${seconds}s`;
+    const timeLeftMs = time - Date.now();
 
+    if(timeLeftMs <= 0) {
+      return 'EXPIRED';
+    }
+
+    const totalSeconds = Math.floor(timeLeftMs / 1000);
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = Math.floor(totalSeconds % 60);
+  
+    return `${hours}h ${minutes}m ${seconds}s`;
+  
   }
 
   async function fetchNewItems() {
@@ -75,12 +78,14 @@ const NewItems = ({ Slider }) => {
   }
 
   useEffect(() => {
+    setTimeout(() => {
       fetchNewItems();
+    }, 5000); //set time out to view skeleton loading
   }, []);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      updateDate(newItems);
+      updateDate(newItems); //one second interval with updated time for countdown timer
     }, 1000);
     return () => clearInterval(intervalId);
   }, [newItems]);
@@ -96,7 +101,7 @@ const NewItems = ({ Slider }) => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          <Slider {...newItemsSettings}>
+          {newItems && newItems.length > 0 ? (  <Slider {...newItemsSettings}>
             {newItems.map((item, index) => {
               return (
               <div className="nft__item" key={index}>
@@ -154,64 +159,25 @@ const NewItems = ({ Slider }) => {
               </div>
               );
             })}
-          </Slider>
-
+          </Slider> ) : ( <Slider {...newItemsSettings}>
           {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
+        
+              <div className="nft__item" key={index}>
                 <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={AuthorImage} alt="" />
+                    <div className="lazy skeleton-box" style={{width: '55px',height:'55px',borderRadius: '50%'}}></div>
                     <i className="fa fa-check"></i>
-                  </Link>
                 </div>
-                <div className="de_countdown">5h 30m 32s</div>
-
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-
-                  <Link to="/item-details">
-                    <img
-                      src={AuthorImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
+                <div className="nft__item_wrap skeleton-box" style={{width: '100%',height:'174px', marginBottom: '12px'}}>
                 </div>
                 <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>69</span>
-                  </div>
+                    <h4 className="skeleton-box" style={{width: '80%'}}></h4>
+                  <div className="nft__item_price skeleton-box" style={{width: '50%'}}></div>
                 </div>
               </div>
-            </div>
+
           ))}
+          </Slider>)}
+        
         </div>
       </div>
     </section>
