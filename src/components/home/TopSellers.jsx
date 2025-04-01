c83fbb9a01e51527328b840c26701a6f5188d8fc
Adria <adriamooney@gmail.com>
2025-04-01 13:22:27 -0700

    completed top sellers feature.  pull top sellers from api using axios and display skeleton loading state for slow connections

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
 
 const TopSellers = () => {
+
+  const [topSellers, setTopSellers] = useState([]);
+
+  async function fetchTopSellers() {
+    const { data } = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers`
+    );
+    setTopSellers(data);
+  }
+
+  useEffect(() => {
+    fetchTopSellers();
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,24 +29,38 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
+              {topSellers && topSellers.length > 0 ? (topSellers.map((item, index) => {
+                return (
+                  <li key={index}>
                   <div className="author_list_pp">
                     <Link to="/author">
                       <img
                         className="lazy pp-author"
-                        src={AuthorImage}
+                        src={item.authorImage}
                         alt=""
                       />
                       <i className="fa fa-check"></i>
                     </Link>
                   </div>
                   <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
+                    <Link to="/author">{item.authorName}</Link>
+                    <span>{item.price} ETH</span>
                   </div>
                 </li>
-              ))}
+                )
+              })) : ( new Array(12).fill(0).map((_, index) => (
+                <li key={index}>
+                  <div className="author_list_pp">
+                    <div className="pp-author skeleton-box"  style={{width: '55px',height:'55px',borderRadius: '50%'}}></div>
+                      <i className="fa fa-check"></i>
+                  </div>
+                  <div class="author_list_info">
+                    <div className="skeleton-box" style={{width: '100px',height:'20px', marginBottom: '8px', display: 'block'}}></div>
+                    <div className="skeleton-box" style={{width: '45px',height:'20px'}}></div>
+                  </div>
+                </li>
+              )))}
+             
             </ol>
           </div>
         </div>
